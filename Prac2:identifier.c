70b5d86b0472ae8854cf17af9909da0ff73b67d3
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-06-11 22:56:54 +0530

    Update Prac2:identifier.c

--- Prac2:identifier.c
+++ Prac2:identifier.c
@@ -1,1 +1,20 @@
+Program 2: Design a LEX Code to identify and print valid Identifier of C/C++ in given Input
+pattern.
+Source Code
 
+
+%{
+int c=0;
+%}
+%%
+[a-zA-Z_][a-zA-Z0-9_]* {c++; printf("\tvalid Identifier=%s",yytext);}
+. ;
+%%
+int yywrap()
+{
+return 1 ;
+int main()
+{
+yylex();
+printf("\nTotal number of valid Identifier = %d \n",c);
+} 
