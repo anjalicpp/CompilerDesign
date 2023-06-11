80288b8f37fe533b5f7380c24fe10e44a586dd24
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-06-11 22:49:57 +0530

    Update Prac1:word,line,space_count.c

--- Prac1:word,line,space_count.c
+++ Prac1:word,line,space_count.c
@@ -1,1 +1,19 @@
 
+Program 1: Lex code for counting the number of lines, spaces, tabs, the and rest of the characters. 
+Source Code 
+%{ int 
+n,m,t,c; 
+%} 
+%% 
+\n n++; 
+\t m++; 
+[ ] t++; 
+. c++; 
+%% 
+int yywrap() 
+{ 
+return 1; 
+} 
+int main() 
+{ yylex(); 
+printf("Total number of\nlines=%d \ntabs=%d\nspaces=%d\nchars=%d \n",n,m,t,c); }
