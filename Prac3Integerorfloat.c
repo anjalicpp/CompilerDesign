0d321e42c1c3bd13610c3dc9de6d07752f51b3e3
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-06-11 22:51:06 +0530

    Update Prac3Integerorfloat.c

--- Prac3Integerorfloat.c
+++ Prac3Integerorfloat.c
@@ -1,1 +1,21 @@
+Program 3: Design a LEX Code to identify and print integer and float value in given Input
+pattern.
+Source Code 
 
+%{
+int m=0,n=0;
+%}
+%%
+-?[0-9]+ {m++; printf("\t Integer = %s",yytext);}
+-?[0-9]+"."[0-9]+ {n++; printf("\t Float = %s",yytext);}
+. ;
+%%
+int yywrap()
+{
+return 1 ;
+}
+int main()
+{
+yylex();
+printf("\nTotal number of Integer = %d & Float = %d \n",m,n);
+} 
