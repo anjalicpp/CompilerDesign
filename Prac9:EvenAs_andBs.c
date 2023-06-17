ac73d2ba716154aa1da079412b7e55d145e27dc5
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-06-17 23:42:26 +0530

    Update Prac9:EvenAs_andBs.c

--- Prac9:EvenAs_andBs.c
+++ Prac9:EvenAs_andBs.c
@@ -5,27 +5,36 @@ Source Code
 %{ 
 #include<stdio.h> 
 %} 
-%s A B C DEAD 
+%s A B C DEAD
+
 %% 
-<INITIAL>a BEGIN A; <INITIAL>b 
-BEGIN B; 
+<INITIAL>a BEGIN A; 
+<INITIAL>b BEGIN B; 
 <INITIAL>[^ba\n] BEGIN DEAD; 
 <INITIAL>\n BEGIN INITIAL; {printf("Accepted\n");} 
+
 <A>a BEGIN INITIAL; 
 <A>b BEGIN C; 
 <A>[^ba\n] BEGIN DEAD; 
 <A>\n BEGIN INITIAL; {printf("Not Accepted\n");} 
+
 <B>a BEGIN C; 
 <B>b BEGIN INITIAL; 
 <B>[^ba\n] BEGIN DEAD; 
 <B>\n BEGIN INITIAL; {printf("Not Accepted\n");} 
+
 <C>a BEGIN B; <C>b BEGIN A; 
 <C>[^ba\n] BEGIN DEAD; 
 <C>\n BEGIN INITIAL; {printf("Not Accepted\n");} 
-DEAD>[^\n] BEGIN DEAD; 
+<DEAD>[^\n] BEGIN DEAD; 
 <DEAD>\n BEGIN INITIAL; {printf("Invalid\n");} 
 %% 
-int yywrap() {return 1;} int 
-	main() 	{printf("Enter 
-String\n"); yylex(); return 
-0; } 
+int yywrap()
+{return 1;} 
+
+int main() 	
+{
+	printf("Enter String\n");
+	yylex(); 
+	return 0; 
+} 
